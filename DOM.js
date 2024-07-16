021cac765b0df9be4a1a146c60b0d9e42d49a078
Sumalatha213 <22a91a0213@aec.edu.in>
2024-07-16 11:58:10 +0530

    second commit

---
+++ DOM.js
@@ -0,0 +1,16 @@
+// console.log(document.getElementsByClassName("mydiv")[3])
+// function fun()
+// {
+//     document.getElementsByClassName("mydiv")[0].innerHTML="suma"
+// }
+
+
+// console.log(document.getElementsByClassName("hai")[2].innerHTML="suma")
+
+function myfun()
+{
+    console.log("coming")
+     var x = document.getElementsByClassName("mydiv")[0];
+     x.setAttribute("src","https://vignette.wikia.nocookie.net/disney/images/5/55/Sofia_9.jpg/revision/latest?cb=20161001180408");
+}
+
